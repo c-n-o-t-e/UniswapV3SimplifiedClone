63b8b60797b8e75eecf188f192beebd39fa4b046
c-note <dongustavo20@gmail.com>
2023-09-28 14:32:30 +0100

    Create library.

---
+++ src/lib/Position.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+library Position {
+    struct Info {
+        uint128 liquidity;
+    }
+
+    // src/libs/Position.sol
+    function update(Info storage self, uint128 liquidityDelta) internal {
+        uint128 liquidityBefore = self.liquidity;
+        uint128 liquidityAfter = liquidityBefore + liquidityDelta;
+
+        self.liquidity = liquidityAfter;
+    }
+
+    function get(
+        mapping(bytes32 => Info) storage self,
+        address owner,
+        int24 lowerTick,
+        int24 upperTick
+    ) internal view returns (Position.Info storage position) {
+        position = self[
+            keccak256(abi.encodePacked(owner, lowerTick, upperTick))
+        ];
+    }
+}
