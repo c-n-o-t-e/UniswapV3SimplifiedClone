dd013d229b4bb1fd78604e48a47a8b719edff1b1
c-note <dongustavo20@gmail.com>
2023-10-01 06:14:46 +0100

    Update struct, and test cases.

--- test/UniswapV3Pool.t.sol
+++ test/UniswapV3Pool.t.sol
@@ -14,8 +14,6 @@ contract UniswapV3PoolTest is Test, TestUtils {
     UniswapV3Manager manager;
 
     struct TestCaseParams {
-        uint256 wethBalance;
-        uint256 usdcBalance;
         int24 currentTick;
         int24 lowerTick;
         int24 upperTick;
@@ -34,8 +32,6 @@ contract UniswapV3PoolTest is Test, TestUtils {
 
     function testMintSuccess() public {
         TestCaseParams memory params = TestCaseParams({
-            wethBalance: 1 ether,
-            usdcBalance: 5000 ether,
             currentTick: 85176,
             lowerTick: 84222,
             upperTick: 86129,
@@ -44,20 +40,15 @@ contract UniswapV3PoolTest is Test, TestUtils {
             mintLiqudity: true
         });
 
-        (uint256 poolBalance0, uint256 poolBalance1) = setupTestCase(params);
+        (
+            uint256 expectedAmount0,
+            uint256 expectedAmount1,
+            uint160 sqrtPriceX96,
+            int24 tick
+        ) = setupTestCase(params);
 
-        (uint160 sqrtPriceX96, int24 tick) = pool.slot0();
-
-        uint256 expectedAmount0 = Math.calcAmount0Delta(
-            TickMath.getSqrtRatioAtTick(tick),
-            TickMath.getSqrtRatioAtTick(params.upperTick),
-            params.liquidity
-        );
-        uint256 expectedAmount1 = Math.calcAmount1Delta(
-            TickMath.getSqrtRatioAtTick(tick),
-            TickMath.getSqrtRatioAtTick(params.lowerTick),
-            params.liquidity
-        );
+        uint256 poolBalance0 = token0.balanceOf(address(pool));
+        uint256 poolBalance1 = token1.balanceOf(address(pool));
 
         assertEq(
             poolBalance0,
@@ -101,81 +92,93 @@ contract UniswapV3PoolTest is Test, TestUtils {
         );
     }
 
-    // function testSwapBuyEth() public {
-    //     TestCaseParams memory params = TestCaseParams({
-    //         wethBalance: 1 ether,
-    //         usdcBalance: 5000 ether,
-    //         currentTick: 85176,
-    //         lowerTick: 84222,
-    //         upperTick: 86129,
-    //         liquidity: 1517882343751509868544,
-    //         currentSqrtP: 5602277097478614198912276234240,
-    //         mintLiqudity: true
-    //     });
+    function testSwapBuyEth() public {
+        TestCaseParams memory params = TestCaseParams({
+            currentTick: 85176,
+            lowerTick: 84222,
+            upperTick: 86129,
+            liquidity: 1517882343751509868544,
+            currentSqrtP: 5602277097478614198912276234240,
+            mintLiqudity: true
+        });
 
-    //     UniswapV3Pool.CallbackData memory extra = UniswapV3Pool.CallbackData({
-    //         token0: address(token0),
-    //         token1: address(token1),
-    //         payer: address(this)
-    //     });
+        UniswapV3Pool.CallbackData memory extra = UniswapV3Pool.CallbackData({
+            token0: address(token0),
+            token1: address(token1),
+            payer: address(this)
+        });
 
-    //     (uint256 poolBalance0, uint256 poolBalance1) = setupTestCase(params);
+        setupTestCase(params);
 
-    //     token1.mint(address(this), 42 ether);
-    //     int userBalance0Before = int(token0.balanceOf(address(this)));
+        token1.mint(address(this), 42 ether);
+        int userBalance0Before = int(token0.balanceOf(address(this)));
+        int userBalance1Before = int256(token1.balanceOf(address(this)));
 
-    //     token1.approve(address(manager), 42 ether);
+        uint256 poolBalance0 = token0.balanceOf(address(pool));
+        uint256 poolBalance1 = token1.balanceOf(address(pool));
 
-    //     manager.swap(address(pool), abi.encode(extra));
-    //     int amount0Delta = -0.008396714242162444 ether;
-    //     int amount1Delta = 42 ether;
+        uint256 swapAmount = 42 ether;
+        token1.approve(address(manager), 42 ether);
 
-    //     assertEq(
-    //         token0.balanceOf(address(this)),
-    //         uint256(userBalance0Before - amount0Delta),
-    //         "invalid user ETH balance"
-    //     );
+        (int256 amount0Delta, int256 amount1Delta) = manager.swap(
+            address(pool),
+            false,
+            swapAmount,
+            abi.encode(extra)
+        );
 
-    //     assertEq(
-    //         token1.balanceOf(address(this)),
-    //         0,
-    //         "invalid user USDC balance"
-    //     );
+        assertEq(
+            token0.balanceOf(address(this)),
+            uint256(userBalance0Before - amount0Delta),
+            "invalid user ETH balance"
+        );
 
-    //     assertEq(
-    //         token0.balanceOf(address(pool)),
-    //         uint256(int256(poolBalance0) + amount0Delta),
-    //         "invalid pool ETH balance"
-    //     );
-    //     assertEq(
-    //         token1.balanceOf(address(pool)),
-    //         uint256(int256(poolBalance1) + amount1Delta),
-    //         "invalid pool USDC balance"
-    //     );
+        assertEq(
+            token0.balanceOf(address(pool)),
+            uint256(int256(poolBalance0) + amount0Delta),
+            "invalid pool ETH balance"
+        );
 
-    //     (uint160 sqrtPriceX96, int24 tick) = pool.slot0();
+        assertEq(
+            token1.balanceOf(address(this)),
+            uint256(userBalance1Before - amount1Delta),
+            "invalid user USDC balance"
+        );
 
-    //     assertEq(
-    //         sqrtPriceX96,
-    //         5604469350942327889444743441197,
-    //         "invalid current sqrtP"
-    //     );
+        assertEq(
+            token1.balanceOf(address(pool)),
+            uint256(int256(poolBalance1) + amount1Delta),
+            "invalid pool USDC balance"
+        );
 
-    //     assertEq(tick, 85184, "invalid current tick");
+        (uint160 sqrtPriceX96, int24 tick) = pool.slot0();
 
-    //     assertEq(
-    //         pool.liquidity(),
-    //         1517882343751509868544,
-    //         "invalid current liquidity"
-    //     );
-    // }
+        assertEq(
+            sqrtPriceX96,
+            5604469350942327889444743441197,
+            "invalid current sqrtP"
+        );
+
+        assertEq(tick, 85184, "invalid current tick");
+
+        assertEq(
+            pool.liquidity(),
+            params.liquidity,
+            "invalid current liquidity"
+        );
+    }
 
     function setupTestCase(
         TestCaseParams memory params
-    ) internal returns (uint256 poolBalance0, uint256 poolBalance1) {
-        token0.mint(address(this), params.wethBalance);
-        token1.mint(address(this), params.usdcBalance);
-
+    )
+        internal
+        returns (
+            uint256 wethBalance,
+            uint256 usdcBalance,
+            uint160 sqrtPriceX96,
+            int24 tick
+        )
+    {
         pool = new UniswapV3Pool(
             address(token0),
             address(token1),
@@ -192,8 +195,25 @@ contract UniswapV3PoolTest is Test, TestUtils {
         });
 
         if (params.mintLiqudity) {
-            token0.approve(address(manager), params.wethBalance);
-            token1.approve(address(manager), params.usdcBalance);
+            (sqrtPriceX96, tick) = pool.slot0();
+
+            wethBalance = Math.calcAmount0Delta(
+                TickMath.getSqrtRatioAtTick(tick),
+                TickMath.getSqrtRatioAtTick(params.upperTick),
+                params.liquidity
+            );
+
+            usdcBalance = Math.calcAmount1Delta(
+                TickMath.getSqrtRatioAtTick(tick),
+                TickMath.getSqrtRatioAtTick(params.lowerTick),
+                params.liquidity
+            );
+
+            token0.mint(address(this), wethBalance);
+            token1.mint(address(this), usdcBalance);
+
+            token0.approve(address(manager), wethBalance);
+            token1.approve(address(manager), usdcBalance);
 
             manager.mint(
                 address(pool),
@@ -203,8 +223,5 @@ contract UniswapV3PoolTest is Test, TestUtils {
                 abi.encode(extra)
             );
         }
-
-        poolBalance0 = token0.balanceOf(address(pool));
-        poolBalance1 = token1.balanceOf(address(pool));
     }
 }
