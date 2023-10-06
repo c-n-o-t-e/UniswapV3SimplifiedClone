598413f22050915ffd0c4e62ac64106ea808a59f
c-note <dongustavo20@gmail.com>
2023-10-06 14:15:08 +0100

    Update contract.

--- test/TestUtils.sol
+++ test/TestUtils.sol
@@ -1,13 +1,231 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.14;
+pragma solidity 0.8.19;
 
+import "forge-std/Test.sol";
+import "abdk-libraries-solidity/ABDKMath64x64.sol";
+
+import "../src/interfaces/IUniswapV3Pool.sol";
+import "../src/interfaces/IUniswapV3Manager.sol";
+import "../src/lib/FixedPoint96.sol";
+import "../src/UniswapV3Factory.sol";
 import "../src/UniswapV3Pool.sol";
 
-abstract contract TestUtils {
+import "./ERC20Mintable.sol";
+
+abstract contract TestUtils is Test {
+    struct ExpectedStateAfterMint {
+        UniswapV3Pool pool;
+        ERC20Mintable token0;
+        ERC20Mintable token1;
+        uint256 amount0;
+        uint256 amount1;
+        int24 lowerTick;
+        int24 upperTick;
+        uint128 positionLiquidity;
+        uint128 currentLiquidity;
+        uint160 sqrtPriceX96;
+        int24 tick;
+    }
+
+    function divRound(
+        int128 x,
+        int128 y
+    ) internal pure returns (int128 result) {
+        int128 quot = ABDKMath64x64.div(x, y);
+        result = quot >> 64;
+
+        // Check if remainder is greater than 0.5
+        if (quot % 2 ** 64 >= 0x8000000000000000) {
+            result += 1;
+        }
+    }
+
+    // Implements: https://github.com/Uniswap/v3-sdk/blob/b6cd73a71f8f8ec6c40c130564d3aff12c38e693/src/utils/nearestUsableTick.ts
+    function nearestUsableTick(
+        int24 tick_,
+        uint24 tickSpacing
+    ) internal pure returns (int24 result) {
+        result =
+            int24(divRound(int128(tick_), int128(int24(tickSpacing)))) *
+            int24(tickSpacing);
+
+        if (result < TickMath.MIN_TICK) {
+            result += int24(tickSpacing);
+        } else if (result > TickMath.MAX_TICK) {
+            result -= int24(tickSpacing);
+        }
+    }
+
+    function sqrtP(uint256 price) internal pure returns (uint160) {
+        return
+            uint160(
+                int160(
+                    ABDKMath64x64.sqrt(int128(int256(price << 64))) <<
+                        (FixedPoint96.RESOLUTION - 64)
+                )
+            );
+    }
+
+    // Calculates sqrtP from price with tick spacing equal to 60;
+    function sqrtP60(uint256 price) internal pure returns (uint160) {
+        return TickMath.getSqrtRatioAtTick(tick60(price));
+    }
+
+    // Calculates sqrtP from tick with tick spacing equal to 60;
+    function sqrtP60FromTick(int24 tick_) internal pure returns (uint160) {
+        return TickMath.getSqrtRatioAtTick(nearestUsableTick(tick_, 60));
+    }
+
+    function tick(uint256 price) internal pure returns (int24 tick_) {
+        tick_ = TickMath.getTickAtSqrtRatio(sqrtP(price));
+    }
+
+    // Calculates tick from price with tick spacing equal to 60;
+    function tick60(uint256 price) internal pure returns (int24 tick_) {
+        tick_ = tick(price);
+        tick_ = nearestUsableTick(tick_, 60);
+    }
+
+    function sqrtPToNearestTick(
+        uint160 sqrtP_,
+        uint24 tickSpacing
+    ) internal pure returns (int24 tick_) {
+        tick_ = TickMath.getTickAtSqrtRatio(sqrtP_);
+        tick_ = nearestUsableTick(tick_, tickSpacing);
+    }
+
+    function assertMintState(ExpectedStateAfterMint memory expected) internal {
+        assertEq(
+            expected.token0.balanceOf(address(expected.pool)),
+            expected.amount0,
+            "incorrect token0 balance of pool"
+        );
+        assertEq(
+            expected.token1.balanceOf(address(expected.pool)),
+            expected.amount1,
+            "incorrect token1 balance of pool"
+        );
+
+        bytes32 positionKey = keccak256(
+            abi.encodePacked(
+                address(this),
+                expected.lowerTick,
+                expected.upperTick
+            )
+        );
+        uint128 posLiquidity = expected.pool.positions(positionKey);
+        assertEq(
+            posLiquidity,
+            expected.positionLiquidity,
+            "incorrect position liquidity"
+        );
+
+        (
+            bool tickInitialized,
+            uint128 tickLiquidityGross,
+            int128 tickLiquidityNet
+        ) = expected.pool.ticks(expected.lowerTick);
+        assertTrue(tickInitialized);
+        assertEq(
+            tickLiquidityGross,
+            expected.positionLiquidity,
+            "incorrect lower tick gross liquidity"
+        );
+        assertEq(
+            tickLiquidityNet,
+            int128(expected.positionLiquidity),
+            "incorrect lower tick net liquidity"
+        );
+
+        (tickInitialized, tickLiquidityGross, tickLiquidityNet) = expected
+            .pool
+            .ticks(expected.upperTick);
+        assertTrue(tickInitialized);
+        assertEq(
+            tickLiquidityGross,
+            expected.positionLiquidity,
+            "incorrect upper tick gross liquidity"
+        );
+        assertEq(
+            tickLiquidityNet,
+            -int128(expected.positionLiquidity),
+            "incorrect upper tick net liquidity"
+        );
+
+        assertTrue(tickInBitMap(expected.pool, expected.lowerTick));
+        assertTrue(tickInBitMap(expected.pool, expected.upperTick));
+
+        (uint160 sqrtPriceX96, int24 currentTick) = expected.pool.slot0();
+        assertEq(sqrtPriceX96, expected.sqrtPriceX96, "invalid current sqrtP");
+        assertEq(currentTick, expected.tick, "invalid current tick");
+        assertEq(
+            expected.pool.liquidity(),
+            expected.currentLiquidity,
+            "invalid current liquidity"
+        );
+    }
+
+    struct ExpectedStateAfterSwap {
+        UniswapV3Pool pool;
+        ERC20Mintable token0;
+        ERC20Mintable token1;
+        uint256 userBalance0;
+        uint256 userBalance1;
+        uint256 poolBalance0;
+        uint256 poolBalance1;
+        uint160 sqrtPriceX96;
+        int24 tick;
+        uint128 currentLiquidity;
+    }
+
+    function assertSwapState(ExpectedStateAfterSwap memory expected) internal {
+        assertEq(
+            expected.token0.balanceOf(address(this)),
+            uint256(expected.userBalance0),
+            "invalid user ETH balance"
+        );
+        assertEq(
+            expected.token1.balanceOf(address(this)),
+            uint256(expected.userBalance1),
+            "invalid user USDC balance"
+        );
+
+        assertEq(
+            expected.token0.balanceOf(address(expected.pool)),
+            uint256(expected.poolBalance0),
+            "invalid pool ETH balance"
+        );
+        assertEq(
+            expected.token1.balanceOf(address(expected.pool)),
+            uint256(expected.poolBalance1),
+            "invalid pool USDC balance"
+        );
+
+        (uint160 sqrtPriceX96, int24 currentTick) = expected.pool.slot0();
+        assertEq(sqrtPriceX96, expected.sqrtPriceX96, "invalid current sqrtP");
+        assertEq(currentTick, expected.tick, "invalid current tick");
+        assertEq(
+            expected.pool.liquidity(),
+            expected.currentLiquidity,
+            "invalid current liquidity"
+        );
+    }
+
     function encodeError(
         string memory error
     ) internal pure returns (bytes memory encoded) {
         encoded = abi.encodeWithSignature(error);
+    }
+
+    function encodeSlippageCheckFailed(
+        uint256 amount0,
+        uint256 amount1
+    ) internal pure returns (bytes memory encoded) {
+        encoded = abi.encodeWithSignature(
+            "SlippageCheckFailed(uint256,uint256)",
+            amount0,
+            amount1
+        );
     }
 
     function encodeExtra(
@@ -17,11 +235,79 @@ abstract contract TestUtils {
     ) internal pure returns (bytes memory) {
         return
             abi.encode(
-                UniswapV3Pool.CallbackData({
+                IUniswapV3Pool.CallbackData({
                     token0: token0_,
                     token1: token1_,
                     payer: payer
                 })
             );
+    }
+
+    function tickInBitMap(
+        UniswapV3Pool pool,
+        int24 tick_
+    ) internal view returns (bool initialized) {
+        tick_ /= int24(pool.tickSpacing());
+
+        int16 wordPos = int16(tick_ >> 8);
+        uint8 bitPos = uint8(uint24(tick_ % 256));
+
+        uint256 word = pool.tickBitmap(wordPos);
+
+        initialized = (word & (1 << bitPos)) != 0;
+    }
+
+    function mintParams(
+        address tokenA,
+        address tokenB,
+        uint256 lowerPrice,
+        uint256 upperPrice,
+        uint256 amount0,
+        uint256 amount1
+    ) internal pure returns (IUniswapV3Manager.MintParams memory params) {
+        params = IUniswapV3Manager.MintParams({
+            tokenA: tokenA,
+            tokenB: tokenB,
+            tickSpacing: 60,
+            lowerTick: tick60(lowerPrice),
+            upperTick: tick60(upperPrice),
+            amount0Desired: amount0,
+            amount1Desired: amount1,
+            amount0Min: 0,
+            amount1Min: 0
+        });
+    }
+
+    function mintParams(
+        address tokenA,
+        address tokenB,
+        uint160 lowerSqrtP,
+        uint160 upperSqrtP,
+        uint256 amount0,
+        uint256 amount1,
+        uint24 tickSpacing
+    ) internal pure returns (IUniswapV3Manager.MintParams memory params) {
+        params = IUniswapV3Manager.MintParams({
+            tokenA: tokenA,
+            tokenB: tokenB,
+            tickSpacing: tickSpacing,
+            lowerTick: sqrtPToNearestTick(lowerSqrtP, tickSpacing),
+            upperTick: sqrtPToNearestTick(upperSqrtP, tickSpacing),
+            amount0Desired: amount0,
+            amount1Desired: amount1,
+            amount0Min: 0,
+            amount1Min: 0
+        });
+    }
+
+    function deployPool(
+        UniswapV3Factory factory,
+        address token0,
+        address token1,
+        uint24 tickSpacing,
+        uint256 currentPrice
+    ) internal returns (UniswapV3Pool pool) {
+        pool = UniswapV3Pool(factory.createPool(token0, token1, tickSpacing));
+        pool.initialize(sqrtP(currentPrice));
     }
 }
