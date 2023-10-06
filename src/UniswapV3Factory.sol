dbd6f2eb5c373a5d61b86110293a50df4024c1c3
c-note <dongustavo20@gmail.com>
2023-10-06 14:06:34 +0100

    Update import file.

--- src/UniswapV3Factory.sol
+++ src/UniswapV3Factory.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.14;
+pragma solidity 0.8.19;
 
-import "./Interfaces/IUniswapV3PoolDeployer.sol";
 import "./UniswapV3Pool.sol";
 
 contract UniswapV3Factory is IUniswapV3PoolDeployer {
