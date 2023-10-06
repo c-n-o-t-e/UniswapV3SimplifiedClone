a6cbd214407cd13771438a8031cb0ba4527a3f7a
c-note <dongustavo20@gmail.com>
2023-10-06 13:45:16 +0100

    Add Factory contract.

---
+++ src/UniswapV3Factory.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.14;
+
+import "./Interfaces/IUniswapV3PoolDeployer.sol";
+import "./UniswapV3Pool.sol";
+
+contract UniswapV3Factory is IUniswapV3PoolDeployer {
+    error PoolAlreadyExists();
+    error ZeroAddressNotAllowed();
+    error TokensMustBeDifferent();
+    error UnsupportedTickSpacing();
+
+    event PoolCreated(
+        address indexed token0,
+        address indexed token1,
+        uint24 indexed tickSpacing,
+        address pool
+    );
+
+    PoolParameters public parameters;
+
+    mapping(uint24 => bool) public tickSpacings;
+    mapping(address => mapping(address => mapping(uint24 => address)))
+        public pools;
+
+    constructor() {
+        tickSpacings[10] = true;
+        tickSpacings[60] = true;
+    }
+
+    function createPool(
+        address tokenX,
+        address tokenY,
+        uint24 tickSpacing
+    ) public returns (address pool) {
+        if (tokenX == tokenY) revert TokensMustBeDifferent();
+        if (!tickSpacings[tickSpacing]) revert UnsupportedTickSpacing();
+
+        (tokenX, tokenY) = tokenX < tokenY
+            ? (tokenX, tokenY)
+            : (tokenY, tokenX);
+
+        if (tokenX == address(0)) revert ZeroAddressNotAllowed();
+        if (pools[tokenX][tokenY][tickSpacing] != address(0))
+            revert PoolAlreadyExists();
+
+        parameters = PoolParameters({
+            factory: address(this),
+            token0: tokenX,
+            token1: tokenY,
+            tickSpacing: tickSpacing
+        });
+
+        pool = address(
+            new UniswapV3Pool{
+                salt: keccak256(abi.encodePacked(tokenX, tokenY, tickSpacing))
+            }()
+        );
+
+        delete parameters;
+
+        pools[tokenX][tokenY][tickSpacing] = pool;
+        pools[tokenY][tokenX][tickSpacing] = pool;
+
+        emit PoolCreated(tokenX, tokenY, tickSpacing, pool);
+    }
+}
